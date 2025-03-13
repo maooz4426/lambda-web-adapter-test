a7453a2afbce8e6555e84470421f1f9670300921
MAOZ <maooz4426@gmail.com>
2025-03-14 03:35:29 +0900

    test

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+
+
 func main() {
 	e := echo.New()
 
--- tf/lambda.tf
+++ tf/lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "lambda-web-adapter" {
   function_name = "lambda-web-adapter"
   image_uri = "${aws_ecr_repository.lambda-web-adapter.repository_url}:latest"
-  role          = aws_iam_role.iam_for_lambda.arn
+  # role          = aws_iam_role.iam_for_lambda.arn
   package_type  = "Image"
   architectures = ["arm64"]
 }
@@ -11,21 +11,21 @@ resource "aws_lambda_function_url" "test_latest" {
   authorization_type = "NONE"
 }
 
-
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
+#
+# resource "aws_iam_role" "iam_for_lambda" {
+#   name               = "iam_for_lambda"
+#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+# }
+#
+# data "aws_iam_policy_document" "assume_role" {
+#   statement {
+#     effect = "Allow"
+#
+#     principals {
+#       type        = "Service"
+#       identifiers = ["lambda.amazonaws.com"]
+#     }
+#
+#     actions = ["sts:AssumeRole"]
+#   }
+# }
