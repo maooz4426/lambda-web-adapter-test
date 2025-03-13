3deab04984cb2e975c667e26c8511fbb717e1069
MAOZ <maooz4426@gmail.com>
2025-03-14 03:39:45 +0900

    test

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -11,21 +11,21 @@ resource "aws_lambda_function_url" "test_latest" {
   authorization_type = "NONE"
 }
 
-#
-# resource "aws_iam_role" "iam_for_lambda" {
-#   name               = "iam_for_lambda"
-#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
-# }
-#
-# data "aws_iam_policy_document" "assume_role" {
-#   statement {
-#     effect = "Allow"
-#
-#     principals {
-#       type        = "Service"
-#       identifiers = ["lambda.amazonaws.com"]
-#     }
-#
-#     actions = ["sts:AssumeRole"]
-#   }
-# }
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
