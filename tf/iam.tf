891512d9089211a8f983b22e3d959d3ed3a8bf07
MAOZ <maooz4426@gmail.com>
2025-03-15 02:25:08 +0900

    iam test

---
+++ tf/iam.tf
@@ -0,0 +1,69 @@
+resource "aws_iam_openid_connect_provider" "token_actions_github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+}
+
+resource "aws_iam_role" "from_github_lambda_web_adapter_test" {
+  name = "from_github_lambda_web_adapter_test"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Federated": "arn:aws:iam::590183858431:oidc-provider/token.actions.githubusercontent.com"
+            },
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Condition": {
+                "StringEquals": {
+                    "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+                },
+                "StringLike": {
+                    "token.actions.githubusercontent.com:sub": "repo:maooz4426/lambda-web-adapter-test:ref:refs/heads/*"
+                }
+            }
+        }
+    ]
+})
+
+
+}
+
+
+resource "aws_iam_role_policy" "lambda_web_adapter_test" {
+  name = "lambda_web_adapter_test"
+  role = aws_iam_role.from_github_lambda_web_adapter_test.id
+
+  policy = jsonencode({
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Effect": "Allow",
+			"Action": "ecr:GetAuthorizationToken",
+			"Resource": "*"
+		},
+		{
+			"Effect": "Allow",
+			"Action": [
+				"ecr:UploadLayerPart",
+				"ecr:PutImage",
+				"ecr:InitiateLayerUpload",
+				"ecr:CompleteLayerUpload",
+				"ecr:BatchGetImage",
+				"ecr:BatchCheckLayerAvailability"
+			],
+			"Resource": aws_ecr_repository.lambda-web-adapter.arn
+		},
+		{
+			"Effect": "Allow",
+			"Action": "lambda:UpdateFunctionCode",
+			"Resource": "*"
+		}
+	]
+})
+}
